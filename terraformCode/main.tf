054e9d018fa1846ddc93f98398223ccb83bd8c22
Xav519 <dupuis_x@yahoo.com>
2026-02-17 16:07:58 -0500

    Added sg module ref in main.tf

--- terraformCode/main.tf
+++ terraformCode/main.tf
@@ -10,3 +10,10 @@ module "vpc" {
   logic_subnets       = var.logic_subnets
   database_subnets    = var.database_subnets
 }
+
+module "security_groups" {
+  source = "../modules/security-groups"
+
+  project_name = var.project_name
+  vpc_id      = module.vpc.vpc_id
+}
