c4fcec48fdbd2f0c0a98e95298df6b4049526d26
Xav519 <dupuis_x@yahoo.com>
2026-02-17 20:52:03 -0500

    Added alb code module ref in main.tf

--- terraformCode/main.tf
+++ terraformCode/main.tf
@@ -17,3 +17,29 @@ module "security_groups" {
   project_name = var.project_name
   vpc_id      = module.vpc.vpc_id
 }
+
+# External ALB
+module "external_alb" {
+  source           = "./modules/alb"
+  project_name     = var.project_name
+  alb_name         = "external-alb"
+  alb_type         = "external"
+  vpc_id           = module.vpc.vpc_id
+  subnet_ids       = module.vpc.public_subnets
+  security_groups  = [module.sg.external_alb_sg_id]
+  listener_ports   = [80, 443]
+  target_group_name = "presentation-tg"
+}
+
+# Internal ALB
+module "internal_alb" {
+  source           = "./modules/alb"
+  project_name     = var.project_name
+  alb_name         = "internal-alb"
+  alb_type         = "internal"
+  vpc_id           = module.vpc.vpc_id
+  subnet_ids       = module.vpc.presentation_subnets
+  security_groups  = [module.sg.internal_alb_sg_id]
+  listener_ports   = [80]
+  target_group_name = "logic-tg"
+}
