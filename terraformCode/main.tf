1a68e060b090d5201825cd44e42272355109046f
Xav519 <dupuis_x@yahoo.com>
2026-02-17 21:12:49 -0500

    Added rds ref module code in main.tf file

--- terraformCode/main.tf
+++ terraformCode/main.tf
@@ -93,3 +93,25 @@ module "backend_asg" {
   max_size          = 4
 }
 
+# RDS Instance
+module "rds" {
+  source = "./modules/rds"
+
+  project_name = "three-tier"
+
+  db_subnet_ids = module.vpc.database_subnets
+
+  vpc_security_group_ids = [
+    module.sg.rds_main_sg_id
+  ]
+
+  engine            = "mysql"
+  engine_version    = "8.0"
+  instance_class    = "db.t3.micro"
+  allocated_storage = 20
+
+  db_name  = "appdb"
+  username = "admin"
+  password = "StrongPassword123!"
+}
+
