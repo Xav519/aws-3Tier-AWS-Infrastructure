d60ab48af714f25d6ab7d7f188aae1086c6470ce
Xav519 <dupuis_x@yahoo.com>
2026-02-28 15:22:25 -0500

    Fix code issue

--- terraformCode/output.tf
+++ terraformCode/output.tf
@@ -4,11 +4,6 @@ output "external_alb_dns_name" {
    value = module.external_alb.alb_dns_name
 }
 
-# If you want the Bastion Host's Public IP
-output "bastion_public_ip" {
-   value = module.bastion.public_ip
-}
-
 # If you want the Internal (Private) DNS
 output "internal_alb_dns_name" {
    value = module.internal_alb.alb_dns_name
